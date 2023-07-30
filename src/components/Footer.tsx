acc60f4ad1c89854860ac8047c2f6f0de52aa380
msaouab <msaouab@student.1337.ma>
2023-07-30 14:24:20 +0100

    add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,58 @@
 import styled from "styled-components";
 
-const FooterStyle = styled.footer``;
+const FooterStyle = styled.footer`
+	background-color: #000;
+	color: #fff;
+	& > .container {
+		height: 130px;
+		max-width: 1200px;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 2rem;
+		margin: auto;
+		padding: 0 19px;
+		& > ul {
+			display: flex;
+			gap: 1rem;
+			& > li {
+				cursor: pointer;
+				list-style: none;
+			}
+		}
+		& > div {
+			& > p {
+				& > a {
+					color: #fff;
+					text-decoration: none;
+				}
+			}
+		}
+	}
+`;
 
 const Footer = () => {
-	return <FooterStyle></FooterStyle>;
+	return (
+		<FooterStyle>
+			<div className="container">
+				<ul>
+					<li>About</li>
+					<li>Store Locator</li>
+					<li>FAQs</li>
+					<li>News</li>
+					<li>Careers</li>
+					<li>Contact Us</li>
+				</ul>
+				<div>
+					<p>
+						Build by
+						<a href="">{" <msaouab />"}</a>
+					</p>
+				</div>
+			</div>
+		</FooterStyle>
+	);
 };
 
 export default Footer;
