b59b94a2305453374134ddf3b1a2c094750741f8
msaouab <msaouab@student.1337.ma>
2023-08-16 16:13:35 +0100

    fix the routing test

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import Trending from "../components/Trending";
-import Products from "../components/Products";
+// import Products from "../components/Products";
 
 const PageStyle = styled.div`
 	display: flex;
